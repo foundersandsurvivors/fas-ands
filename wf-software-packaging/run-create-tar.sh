fef391c0abca40b3c8d175fe596285e22d1c741b
Claudine Chionh <clchionh@unimelb.edu.au>
2011-11-10 15:14:49 +1100

    What do these scripts do?

--- wf-software-packaging/run-create-tar.sh
+++ wf-software-packaging/run-create-tar.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 echo "# $0 : creating fas ands software package as a tar"
-cd /srv
+
+# If necessary, change to location of output tar file.
+# cd /srv
+
+# Name and location of output tar file.
 TARNAME=fas-ands-1.0.tar
 MYTAR=./fasrepo/x_ands-published-collections/wf-software-packaging/$TARNAME
 tar cfp $MYTAR --exclude=*.swp .fas_environment -C /srv fasrepo/x_ands-published-collections/wf-jobtest fasrepo/x_ands-published-collections/.jobenv  /etc/perl/FAS
